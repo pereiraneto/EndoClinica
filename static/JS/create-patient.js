47805792908ff45ccc8c463616b617d6f242f310
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2018-11-09 10:34:10 -0300

    add fields on create patient

--- static/JS/create-patient.js
+++ static/JS/create-patient.js
@@ -37,7 +37,15 @@ const handleSavePatient = () => {
         name: document.getElementById("patient-name").value,
         cell_phone: document.getElementById("patient-cell-phone").value,
         phone: document.getElementById("patient-phone").value,
-        birth_date: document.getElementById("patient-birth-date").value
+        birth_date: document.getElementById("patient-birth-date").value,
+        email: document.getElementById("patient-email").value,
+        marital_status: document.getElementById("patient-maritualstate").value,
+        gender: document.getElementById("patient-gender").value,
+        job: document.getElementById("patient-job").value,
+        insurance: document.getElementById("patient-insurance").value,
+        insurance_number: document.getElementById("patient-insurancenumber").value,
+        cpf: document.getElementById("patient-cpf").value,
+        rg: document.getElementById("patient-rg").value
     };
     
     console.log(requestBody);
