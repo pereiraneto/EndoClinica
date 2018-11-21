17e0583aff5a2f72e074176e9b10ae7b79f20af5
Régis Melgaço <regis.melgaco@gmail.com>
2018-11-21 10:11:58 -0300

    load patients on patients-list

--- static/JS/list-patients.js
+++ static/JS/list-patients.js
@@ -1,1 +1,29 @@
-console.log('listar pacientes')
+const apiBaseUrl = window.location.origin + "/api/"
+
+
+document.onreadystatechange = () => {
+    if (document.readyState == 'interactive') {
+        requestFromApi(patients => {
+            const patientsTableBody = document.getElementById('patients-table-body')
+
+            patients.forEach(patient => {
+                addTag(tr => {
+                    addTag(th => {
+                        th.scope = "row"
+                        th.textContent = patient.name
+                    }, tr, 'th')
+                    addTag(td => td.textContent = patient.phone, tr)
+                    addTag(td => td.textContent = patient.cell_phone, tr)
+                    addTag(td => td.textContent = new Date().getFullYear() - patient.birth_date.slice(0,4), tr)
+                    addTag(td => {
+                        addTag(a => {
+                            a.textContent = 'Editar'
+                            a.onclick = () =>  console.log('editar', patient)
+                            a.classList = 'default-link'
+                        }, td, 'a')
+                    }, tr)
+                }, patientsTableBody, 'tr')
+            });
+        }, apiBaseUrl + 'pacientes/')
+    }
+}
