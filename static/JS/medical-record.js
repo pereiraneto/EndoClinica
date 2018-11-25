483649823796b650b610af2ae0589502eaa98aa9
jullynobre <jullynobre1@gmail.com>
2018-11-25 19:04:23 -0300

    Add medical-record.js

---
+++ static/JS/medical-record.js
@@ -0,0 +1,32 @@
+
+const fillConsultationHistoryTable = (tableBody, consultations) => {
+    tableBody.innerHTML = '';
+
+    consultations.forEach(consultation => addTag(rowEl => {
+        addTag(th => {
+            th.scope = "row";
+            const date = `${consultation.date.slice(8,10)}/${consultation.date.slice(5,7)}`;
+            const hour = `${consultation.date.slice(11,13)}h${consultation.date.slice(14,16)}`;
+            th.textContent = `${date} - ${hour}`;
+        }, rowEl, "th");
+        console.log(consultation.procedureName)
+        addTag(td => td.textContent = consultation.procedureName, rowEl);
+        addTag(td => {
+            addTag(a => {
+                a.textContent = "Editar";
+                a.classList += "default-link mr-2";
+                a.onclick = () => handleClickInfoModal(consultation);
+            }, td, "a");
+        }, rowEl);
+    }, tableBody, "tr"));
+}
+
+
+document.onreadystatechange = () => {
+    if (!window.indexedDB) {
+        window.alert("Atualize seu navegador para usar este site.");
+    }
+    if (document.readyState == "interactive") {
+        filterPatient(patientId)
+    }
+}
