5ed887aa22a7b252f843c9a5bb166b168ac83e38
Régis Melgaço de Andrade <regis@MacBook-Pro-de-Regis.local>
2018-12-08 12:42:47 -0300

    save anamnese executed exams

--- static/JS/anamnese-details.js
+++ static/JS/anamnese-details.js
@@ -1,29 +1,36 @@
 const baseApiUrl = location.origin + '/api/'
 
 anamneseModelElRelation = [
-    {element_id:"anamnese-date", model_field:"date"},
-    {element_id:"anamnese-maincomplaint", model_field:"main_complaint"},
-    {element_id:"anamnese-hda", model_field:"hda"},
-    {element_id:"anamnese-pathology", model_field:"pathology"},
-    {element_id:"anamnese-comorbidities", model_field:"comorbidities"},
-    {element_id:"anamnese-medications", model_field:"medications"},
-    {element_id:"anamnese-alergies", model_field:"alergies"},
-    {element_id:"anamnese-habits", model_field:"habits"},
-    {element_id:"anamnese-family-history", model_field:"family_history"},
-    {element_id:"anamnese-physical-exam", model_field:"physical_exam"},
-    {element_id:"anamnese-diagnostical-hypothesis", model_field:"diagnostical_hypothesis"},
-    {element_id:"anamnese-conduct", model_field:"conduct"},
-    {element_id:"anamnese-adicional-info", model_field:"adicional_info"},
-    {element_id:"anamnese-insurance", model_field:"insurance"},
-    {element_id:"anamnese-madical-record", model_field:"medical_record"},
-    {element_id:"anamnese-doctor", model_field:"doctor"},
-    {element_id:"anamnese-executed-exams", model_field:"executed_exams"}
+    {elementId:"anamnese-date", modelField:"date"},
+    {elementId:"anamnese-maincomplaint", modelField:"main_complaint"},
+    {elementId:"anamnese-hda", modelField:"hda"},
+    {elementId:"anamnese-pathology", modelField:"pathology"},
+    {elementId:"anamnese-comorbidities", modelField:"comorbidities"},
+    {elementId:"anamnese-medications", modelField:"medications"},
+    {elementId:"anamnese-alergies", modelField:"alergies"},
+    {elementId:"anamnese-habits", modelField:"habits"},
+    {elementId:"anamnese-family-history", modelField:"family_history"},
+    {elementId:"anamnese-physical-exam", modelField:"physical_exam"},
+    {elementId:"anamnese-diagnostical-hypothesis", modelField:"diagnostical_hypothesis"},
+    {elementId:"anamnese-conduct", modelField:"conduct"},
+    {elementId:"anamnese-adicional-info", modelField:"adicional_info"},
+    {elementId:"anamnese-insurance", modelField:"insurance"},
+    {elementId:"anamnese-madical-record", modelField:"medical_record"},
+    {elementId:"anamnese-doctor", modelField:"doctor"},
+    {elementId:"anamnese-executed-exams", modelField:"executed_exams"}
 ]
 
 const handleSaveAnamnese = () => {
     const anamneseData = {}
     anamneseModelElRelation.forEach(field => {
-        anamneseData[field.model_field] = document.getElementById(field.element_id).value
+        el = document.getElementById(field.elementId)
+        if (el.tagName == 'SELECT' && el.multiple){
+            anamneseData[field.modelField] = []
+            el.childNodes.forEach(option => {
+                if(option.selected) anamneseData[field.modelField].push(option.value)
+            })
+        } else
+            anamneseData[field.modelField] = el.value
     });
 
     requestFromApi(baseApiUrl+'anamneses/', undefined, undefined, anamneseData, 'POST')
@@ -47,7 +54,7 @@ document.onreadystatechange = () => {
         if (isEditionView)
             requestFromApi(baseApiUrl+'anamneses/'+anamneseId, anamnese => {
                 anamneseModelElRelation.forEach(field => {
-                    document.getElementById(field.element_id).value = anamnese[field.model_field]
+                    document.getElementById(field.elementId).value = anamnese[field.modelField]
                 })
             })
     }
