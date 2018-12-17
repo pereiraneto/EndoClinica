3df27fea2f7663f84f37918d574d04a4d2d7268b
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2018-12-17 14:59:17 -0300

    save medical report

--- static/JS/new-medical-report.js
+++ static/JS/new-medical-report.js
@@ -3,12 +3,14 @@ const apiBaseUrl = window.location.origin + '/api/'
 const mrNonDinamicFields = [
     {api:'medical_record', elId:'medical-report-patient'},
     {api:'doctor', elId:'medical-report-doctor'},
+    {api:'report_type', elId: 'select-medical-report-template'},
+    {api:'requester', elId:'medical-report-requester'},
     {api:'date', elId:'medical-report-time'},
     {api:'date', elId:'medical-report-date'}
 ]
 
 const handleChangeMRTemplateChange = () => {
-    const mrSelectEl = document.getElementById('select-medical-report-model')
+    const mrSelectEl = document.getElementById('select-medical-report-template')
 
     requestFromApi(`${apiBaseUrl}modelos-laudos/${mrSelectEl.value}`, mrTemplate => {
         Object.keys(mrTemplate.json_template).forEach(field => {
@@ -21,15 +23,41 @@ const handleChangeMRTemplateChange = () => {
                         h6.innerText = field[0].toUpperCase() + field.slice(1)
                     }, col, 'h6')
 
-                    addTag(input => {
-                        input.name = field
-                        input.value = mrTemplate.json_template[field]
-                        input.className = 'form-control'
-                    }, col, 'input')
+                    addTag(textarea => {
+                        textarea.name = field
+                        textarea.value = mrTemplate.json_template[field]
+                        textarea.className = 'form-control mr-form-dynamicfield'
+                        textarea.rows = 5
+                    }, col, 'textarea')
                 }, row, 'div')
             }, document.getElementById('medical-report-custom-fields'), 'div')
         });
     })
+}
+
+const handleSaveMedicalReport = () => {
+    const requestBody = {}
+    mrNonDinamicFields.forEach(field => {
+        requestBody[field.api] = document.getElementById(field.elId).value
+    })
+    requestBody.date += `T${document.getElementById('medical-report-time').value}:00Z`
+
+    const jsonMedicalReport = {}
+
+    const dynamicFields = document.getElementsByClassName('mr-form-dynamicfield')
+    for (let i = 0; i < dynamicFields.length; i++) {
+        const field = dynamicFields[i]
+        jsonMedicalReport[field.name] = field.value
+    }
+    requestBody.json_medical_report = jsonMedicalReport
+
+    requestFromApi(`${apiBaseUrl}laudos/`, response => {
+        window.alert('Salvo com sucesso!')
+        window.location.href = document.referrer
+    }, response => {
+        window.alert('Salvo SEM sucesso!')
+        // mostrar campos
+    }, requestBody, 'POST')
 }
 
 document.onreadystatechange = () => {
