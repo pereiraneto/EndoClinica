fa00e4eac76310be65b6948a7bf8d766ee16c61b
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2019-01-23 11:45:19 -0300

    feedback on save new recommendation

--- static/JS/medical-recommendation.js
+++ static/JS/medical-recommendation.js
@@ -15,7 +15,23 @@ const handleSaveMedicalrecommendation = () => {
 
     requestBody.json_medical_recommendation = recommendationQuill.getContents()
 
-    requestFromApi(`${apiBaseUrl}recomendacoes/`, undefined, undefined, requestBody, 'POST');
+    requestFromApi(`${apiBaseUrl}recomendacoes/`, response => {
+        window.alert('Recomendação salva com sucessso')
+
+        window.location.href = document.referrer
+    }, response => {
+        console.log('erro ao salvar:', response)
+
+        window.alert("Erro ao salvar recomendação. Verifique os campos preenchidos.")
+        
+        mrNonDinamicFields.forEach(field => {
+            if (response.hasOwnProperty(field.api)) {
+                document.getElementById(field.elId).className = "form-control consultation-input is-invalid"
+            } else {
+                document.getElementById(field.elId).className = "form-control consultation-input"
+            }
+        })
+    }, requestBody, 'POST');
 
     console.log("requestBody > ", requestBody)
     console.log("recommendationQuill > ", recommendationQuill)
