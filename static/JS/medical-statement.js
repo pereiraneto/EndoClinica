9b31fec1ec08f7b84e32a7473301559dca5c2105
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2019-01-31 09:27:10 -0300

    save new medical statement

--- static/JS/medical-statement.js
+++ static/JS/medical-statement.js
@@ -1,47 +1,50 @@
 const apiBaseUrl = window.location.origin + '/api/'
 
 const mrNonDinamicFields = [
-    {api:'medical_record', elId:'medical-recommendation-patient'},
-    {api:'doctor', elId:'medical-recommendation-doctor'},
-    {api:'date', elId:'medical-recommendation-date'}
+    {api:'medical_record', elId:'medical-statement-patient'},
+    {api:'doctor', elId:'medical-statement-doctor'},
+    {api:'date', elId:'medical-statement-time'},
+    {api:'date', elId:'medical-statement-date'},
 ]
 
 
-const handleSaveMedicalRecommendationTemplate = () => {
-    const requestBody = {
-        "json_medical_recommendation": recommendationTemplateQuill.getContents(),
-        "name": document.getElementById("recommendation-template-name").value,
-        "doctor": document.getElementById("medical-recommendation-doctor").value
-    }
+// const handleSaveMedicalStatementTemplate = () => {
+//     const requestBody = {
+//         "json_medical_statement": statementTemplateQuill.getContents(),
+//         "name": document.getElementById("statement-template-name").value,
+//         "doctor": document.getElementById("medical-statement-doctor").value
+//     }
 
-    requestFromApi(`${apiBaseUrl}modelos-recomendacoes/`, (response) => {
-        window.alert('Modelo de Recomendação salva com sucessso.\nRecarreque esta página para usar o novo template!')
-    }, (response) => {
-        console.log('erro ao salvar:', response)
-        window.alert("Erro ao salvar recomendação. Verifique os campos preenchidos.")
-        document.getElementById("recommendation-template-name").className = "form-control consultation-input is-invalid"
-    }, requestBody, 'POST')
-}
+//     requestFromApi(`${apiBaseUrl}modelos-recomendacoes/`, (response) => {
+//         window.alert('Modelo de Recomendação salva com sucessso.\nRecarreque esta página para usar o novo template!')
+//     }, (response) => {
+//         console.log('erro ao salvar:', response)
+//         window.alert("Erro ao salvar recomendação. Verifique os campos preenchidos.")
+//         document.getElementById("statement-template-name").className = "form-control consultation-input is-invalid"
+//     }, requestBody, 'POST')
+// }
 
 
-const handleSaveMedicalRecommendation = () => {
+const handleSaveMedicalStatement = () => {
     const requestBody = {}
     mrNonDinamicFields.forEach(field => {
         requestBody[field.api] = document.getElementById(field.elId).value
     })
+    requestBody.date += `T${document.getElementById('medical-statement-time').value}:00Z`
 
-    requestBody.json_medical_recommendation = recommendationQuill.getContents()
+    requestBody.json_medical_statement = statementQuill.getContents()
     requestMethod = isEditionView ? 'PUT' : 'POST'
-    requestUrl = isEditionView ? `${apiBaseUrl}recomendacoes/${medicalRecommedationId}/` : `${apiBaseUrl}recomendacoes/`
+    requestUrl = isEditionView ? `${apiBaseUrl}declaracoes/${medicalStatementId}/` : `${apiBaseUrl}declaracoes/`
+
 
     requestFromApi(requestUrl, response => {
-        window.alert('Recomendação salva com sucessso')
+        window.alert('Declaração salva com sucessso')
 
         window.location.href = document.referrer
     }, response => {
         console.log('erro ao salvar:', response)
 
-        window.alert("Erro ao salvar recomendação. Verifique os campos preenchidos.")
+        window.alert("Erro ao salvar declaração. Verifique os campos preenchidos.")
         
         mrNonDinamicFields.forEach(field => {
             if (response.hasOwnProperty(field.api)) {
@@ -53,33 +56,33 @@ const handleSaveMedicalRecommendation = () => {
     }, requestBody, requestMethod);
 }
 
-const handleDeleteRecommendationTemplate = () => {
-    const recommendationId = document.getElementById('recommendation-template-select').value
+// const handleDeleteStatementTemplate = () => {
+//     const statementId = document.getElementById('statement-template-select').value
 
-    requestFromApi(`${apiBaseUrl}modelos-recomendacoes/${recommendationId}`, response => {
-        window.alert("Modelo de Recomendação deletada com sucesso!\nQuando você voltar a esta página, o modelo não estará mais aqui.")
-    }, () => {
-        window.alert("Houve um erro ao tentar deletar esse modelo.")
-    }, undefined, "DELETE")
-}
+//     requestFromApi(`${apiBaseUrl}modelos-recomendacoes/${statementId}`, response => {
+//         window.alert("Modelo de Recomendação deletada com sucesso!\nQuando você voltar a esta página, o modelo não estará mais aqui.")
+//     }, () => {
+//         window.alert("Houve um erro ao tentar deletar esse modelo.")
+//     }, undefined, "DELETE")
+// }
 
-const handleChangeSelectedRecommendationTemplate = () => {
-    const seletedRecommendationTemplateId = document.getElementById('recommendation-template-select').value
+// const handleChangeSelectedstatementTemplate = () => {
+//     const seletedstatementTemplateId = document.getElementById('statement-template-select').value
 
-    if (seletedRecommendationTemplateId != 0) {
-        requestFromApi(`${apiBaseUrl}modelos-recomendacoes/${seletedRecommendationTemplateId}`, recommendationTemplate => {
-            recommendationQuill.setContents(recommendationTemplate.json_medical_recommendation)
-        })
-    }
-}
+//     if (seletedstatementTemplateId != 0) {
+//         requestFromApi(`${apiBaseUrl}modelos-recomendacoes/${seletedstatementTemplateId}`, statementTemplate => {
+//             statementQuill.setContents(statementTemplate.json_medical_statement)
+//         })
+//     }
+// }
 
 
-document.onreadystatechange = () => {
-    if (document.readyState == "interactive") {
-        if (isEditionView) {
-            requestFromApi(`${apiBaseUrl}recomendacoes/${medicalRecommedationId}`, medicalRecommendation => {
-                recommendationQuill.setContents(medicalRecommendation.json_medical_recommendation)
-            })
-        }
-    }
-}
+// document.onreadystatechange = () => {
+//     if (document.readyState == "interactive") {
+//         if (isEditionView) {
+//             requestFromApi(`${apiBaseUrl}recomendacoes/${medicalRecommedationId}`, medicalstatement => {
+//                 statementQuill.setContents(medicalstatement.json_medical_statement)
+//             })
+//         }
+//     }
+// }
